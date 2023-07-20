bce6f348894c25f0f87cf0f18901ec39b4cbc6cc
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-20 11:02:30 +0500

    MVC architecture

--- BasicHttpServerUsingNode/index.js
+++ BasicHttpServerUsingNode/index.js
@@ -4,8 +4,25 @@ const app = express();  // create a express server object
 
 const PORT = 10;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+const myLogger = (req, res, next) => {
+  console.log("loggging from middleware 1")
+  return res.json({msg: "returning"});
+  next();  // calls the next middleware
+}
+
+const extraLogger = (req, res, next) => {
+  console.log("loggging from middleware 2")
+  next(); 
+}
+
+const middleware = [myLogger, extraLogger] // created the array for reusibility..
+
+app.get("/home",middleware, (req, res) => {
+  console.log("Last middleware")
+  res.json({
+    message: "Ok get"
+  })
 });
 
 app.listen(PORT, () => {
